5a4434e7e908b858b98374f3e9a21bdbdfa1a795
Pedro Neto <pedro.neto.704@ufrn.edu.br>
2026-04-13 22:02:50 -0300

    quizzes

--- exercises/quizzes/quiz1.rs
+++ exercises/quizzes/quiz1.rs
@@ -12,6 +12,13 @@
 // the quantity bought.
 // fn calculate_price_of_apples(???) -> ??? { ??? }
 
+fn calculate_price_of_apples(num: i32) -> i32 {
+    if num <= 40 {
+        return num * 2;
+    }
+    num
+}
+
 fn main() {
     // You can optionally experiment here.
 }
